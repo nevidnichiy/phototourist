e45279216519410a0b0d2e50229ad3aa59f10001
Sergey Nevidnichiy <nevidnichiy@gmail.com>
2017-01-23 12:34:35 +0000

    Added mongoid

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,4 @@
-class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+class ApplicationController < ActionController::API
+  # make connection between controller action and associated view
+  include ActionController::ImplicitRender
 end
